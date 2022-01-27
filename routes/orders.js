53ddb9eb336fee0d1e1b321551d16c6a199e48d4
emilyc38 <emilyc38@gmail.com>
2022-01-27 13:02:06 -0600

    added orders methods

---
+++ routes/orders.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+
+const { createOrder, checkoutCart } = require("../db");
+
+const ordersRouter = Router();
+
+//CREATE ORDER
+ordersRouter.patch("/update", async (req, res, next) => {
+  try {
+  } catch (error) {
+    return next(error);
+  }
+});
+
+//CHECKOUT CART
+ordersRouter.patch("/checkout", async (req, res, next) => {
+  try {
+    const { id } = req.body;
+
+    const order = await checkoutCart(id);
+    res.status(200).send(order);
+  } catch (error) {
+    return next(error);
+  }
+});
