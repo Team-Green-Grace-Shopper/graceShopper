a38e498ebc8b945b8e55b6b02fbc592a2a131e4f
emilyc38 <emilyc38@gmail.com>
2022-02-07 01:56:38 -0600

    trying to deploy

--- db/client.js
+++ db/client.js
@@ -1,5 +1,7 @@
 const { Client } = require("pg");
-const client = new Client("postgress://localhost:5432/graceshopper");
+const client = new Client(
+  process.env.DATABASE_URL || "postgress://localhost:5432/graceshopper"
+);
 
 //change
 module.exports = { client };
