65439457ee145ccb95c030192727f68e1ecefa1d
emilyc38 <emilyc38@gmail.com>
2022-02-07 02:16:17 -0600

    fixed typo in client.js

--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require("pg");
 const client = new Client(
-  process.env.DATABASE_URL || "postgress://localhost:5432/graceshopper"
+  process.env.DATABASE_URL || "postgres://localhost:5432/graceshopper"
 );
 
 //change
